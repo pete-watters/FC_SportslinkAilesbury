b397f2fbc32419956e2de586ffe567f0ff2b09e3
Peter Watters <a552722@fmr.com>
2014-08-12 10:31:33 +0100

    Adding files after first Grunt start

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fcSportslinkAilesburyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fcSportslinkAilesburyApp
+ */
+angular.module('fcSportslinkAilesburyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
