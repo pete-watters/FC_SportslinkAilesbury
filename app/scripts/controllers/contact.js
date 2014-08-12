566c8edf6d09ad742a3ce53a3ab416d802fd674a
Peter Watters <a552722@fmr.com>
2014-08-12 14:31:31 +0100

    Adding new styles and restructured CSS

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fcSportslinkAilesburyApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the fcSportslinkAilesburyApp
+ */
+angular.module('fcSportslinkAilesburyApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
