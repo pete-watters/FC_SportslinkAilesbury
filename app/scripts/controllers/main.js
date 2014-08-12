505e042099daad5c9e0e0a9518aeebfb94156c6a
Peter Watters <a552722@fmr.com>
2014-08-12 11:13:53 +0100

    Adding unit testing and sortable ToDos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,19 @@
  */
 angular.module('fcSportslinkAilesburyApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.awesomeThings = ['HTML5 Boilerplate','AngularJS','Karma'];
+
+    $scope.todos = [];
+
+    /* function run when addTodo() pressed */
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    /* function to remove toDos */
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,26 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  /* Test ToDos is not populated at start */
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
   });
+
+  /* Test addTodo works */
+  it('should add items to the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+
+  /* Test remove ToDo works */
+  it('should add then remove an item from the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(0);
+  });
+
+
+
 });
